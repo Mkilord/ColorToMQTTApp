8cd21a791696461c77a0e1fe1d95aa109911c844
Mkilord <mkilord6666@gmail.com>
2025-02-24 00:31:22 +0300

    Add: Properties support

--- src/main/java/ru/mkilord/colortomqttapp/ColorToMqttAppApplication.java
+++ src/main/java/ru/mkilord/colortomqttapp/ColorToMqttAppApplication.java
@@ -3,6 +3,7 @@ package ru.mkilord.colortomqttapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import ru.mkilord.colortomqttapp.config.PropertiesConfig;
 
 @SpringBootApplication
 public class ColorToMqttAppApplication {
---
+++ src/main/java/ru/mkilord/colortomqttapp/config/PropertiesConfig.java
@@ -0,0 +1,35 @@
+package ru.mkilord.colortomqttapp.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import ru.mkilord.colortomqttapp.properties.PropertiesLoader;
+
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.Properties;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Log4j2
+@Getter
+@Configuration
+@ConfigurationProperties(prefix = "app")
+@FieldDefaults(level = PRIVATE)
+public class PropertiesConfig {
+
+    final String settingsFilePath = Path.of(".").toAbsolutePath().resolve("settings.txt").normalize().toString();
+    @Setter
+    Map<String, String> defaultSettings;
+
+    @Primary
+    @Bean
+    public Properties getProperties(PropertiesLoader propertiesLoader) {
+        return propertiesLoader.getPropertiesClone();
+    }
+}

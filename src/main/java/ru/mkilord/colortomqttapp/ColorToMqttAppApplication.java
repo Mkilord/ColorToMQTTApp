2937e8a19e36ba8de2e7454f18c3300d3c90925c
Mkilord <mkilord6666@gmail.com>
2025-03-04 12:59:30 +0300

    Refactor: remove trash code

--- src/main/java/ru/mkilord/colortomqttapp/ColorToMqttAppApplication.java
+++ src/main/java/ru/mkilord/colortomqttapp/ColorToMqttAppApplication.java
@@ -2,16 +2,11 @@ package ru.mkilord.colortomqttapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.mkilord.colortomqttapp.service.SettingsService;
 
 @SpringBootApplication
 public class ColorToMqttAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(ColorToMqttAppApplication.class, args);
-    SettingsService settingsService = null;
-    var properties = settingsService.loadOrElseLoadDefault();
-    settingsService.save(properties);
-
     }
 
 }

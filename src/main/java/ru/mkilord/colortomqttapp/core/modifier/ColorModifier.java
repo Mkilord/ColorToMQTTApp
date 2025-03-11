4dfd6c1d4b99447f21344c0803b2a84fc57b46e5
Mkilord <mkilord6666@gmail.com>
2025-03-11 13:25:19 +0300

    Refactor: clean imports

--- src/main/java/ru/mkilord/colortomqttapp/core/modifier/ColorModifier.java
+++ src/main/java/ru/mkilord/colortomqttapp/core/modifier/ColorModifier.java
@@ -2,8 +2,6 @@ package ru.mkilord.colortomqttapp.core.modifier;
 
 import ru.mkilord.colortomqttapp.core.HSBColor;
 
-import java.awt.*;
-
 public interface ColorModifier {
     HSBColor modify(HSBColor color);
 }
